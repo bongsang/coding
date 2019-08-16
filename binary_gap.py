9c3fc51de46022a881f1c5b638c1319c9f3b1893
Bongsang <happykbs@gmail.com>
2019-08-17 08:50:49 +0900

    binary_gap updated

--- binary_gap.py
+++ binary_gap.py
@@ -1,3 +1,6 @@
+# 1. BinaryGap
+# Find longest sequence of zeros in binary representation of an integer.
+
 """
 A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
@@ -11,34 +14,35 @@ Write an efficient algorithm for the following assumptions:
 N is an integer within the range [1..2,147,483,647].
 """
 
-# Solution
-# def max_gap(x):
-#     max_gap_length = 0
-#     current_gap_length = 0
-#     for i in range(x.bit_length()):
-#         if x & (1 << i):
-#             # Set, any gap is over.
-#             if current_gap_length > max_gap_length:
-#                 max_gap_length = current_gap_length
-#             current_gap_length = 0
-#          else:
-#             # Not set, the gap widens.
-#             current_gap_length += 1
-#     # Gap might end at the end.
-#     if current_gap_length > max_gap_length:
-#         max_gap_length = current_gap_length
-#     return max_gap_length
-
 import numpy as np
 
-def solution(N):
-    bin_N = bin(N).replace("0b", "")
-    print("${N} is ${bin_N}".format())
 
+def solution(N):
+    # write your code in Python 3.6
+    N_bin = bin(N).replace('0b', '')
+    # print(N_bin)
+
+    current_count = 0
+    best_count = 0
+    for i in range(len(N_bin)):
+        checkpoint = N_bin[i]
+        if checkpoint == '1':
+            if current_count > best_count:
+                best_count = current_count
+            current_count = 0
+        elif checkpoint == '0':
+            current_count += 1
+        # print(f'{i}: {current_count}')
+
+    return best_count
 
 
 if __name__ == '__main__':
+    # 561892, 74901729, 1376796946
     np.random.seed(77)
-    N = np.random.randint(1, 647)
-    solution(N)
+    N = np.random.randint(1, 1376796946)
+    N = 561892
+    zero_count = solution(N)
+    print(f'{N}: {bin(N)}: {zero_count}')
+
 
